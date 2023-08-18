f0eef5d97b57eb6f75215984ab19328092cf4cc3
amr-ibrahim7 <amrnew100@gmail.com>
2023-08-18 18:40:54 +0300

    first commit

---
+++ src/features/menu/Menu.jsx
@@ -0,0 +1,22 @@
+import { useLoaderData } from 'react-router-dom';
+import { getMenu } from '../../services/apiRestaurant';
+import MenuItem from './MenuItem';
+
+function Menu() {
+  const menu = useLoaderData();
+  // console.log(menu);
+  return (
+    <ul className="divdide-stone-200 divide-y px-2">
+      {menu.map((pizza) => (
+        <MenuItem pizza={pizza} key={pizza.id} />
+      ))}
+    </ul>
+  );
+}
+
+export async function loader() {
+  const menu = await getMenu();
+  return menu;
+}
+
+export default Menu;
